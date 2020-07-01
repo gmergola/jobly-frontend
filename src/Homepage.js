423268ed26c07087075e6059b38c17507362d8da
Genna Mergola <gennamergola@gmail.com>
2020-07-01 08:13:11 -0700

    changed style

--- src/Homepage.js
+++ src/Homepage.js
@@ -5,7 +5,7 @@ function HomePage(){
   return(
     <div className="Home-container">
       <h1 className="Home-title">Welcome to Jobly!</h1>
-      <h6 className="description text-muted">A place you can find and apply to the job of your dreams...</h6>
+      <div className="description"><i>~ Find and apply to the job of your dreams ~</i></div>
     </div>
   )
 
